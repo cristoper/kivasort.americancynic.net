9726ae665a6044bfb50f6d686d82d3950289caa0
A. Cynic <chris@mretc.net>
2014-12-01 11:20:36 -0700

    Allow user to select which columns are visible
    Uses DataTables ColVis extension (http://datatables.net/extensions/colvis/). It works in conjuction with the Responsive extension.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
     var table = $('#KivaSort').makeKivaTable({
         scrollY: 300,
         autoWidth: true,
-        scrollX: true
+        scrollX: true,
+        dom: 'C<"clear">lfrtip' // ColVis plugin
     }).DataTable();
+
+    table.on('column-visibility.dt', function() {
+        $.fn.dataTable.ColVis.fnRebuild();
     });
 
     $('#tabs').tabs();
