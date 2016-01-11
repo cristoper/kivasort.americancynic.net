5eb7e57c15a29539c391be78e85d54ad68d99b9e
A. Cynic <chris@mretc.net>
2016-01-10 21:03:18 -0700

    Make deploy-to-pages script robust
    - don't allow to run from gh-pages
    - check if there are any changes before attempting to commit changes
    - display message when pushing changes

--- deploy-to-pages.sh
+++ deploy-to-pages.sh
@@ -2,17 +2,35 @@
 set -o errexit
 set -o nounset
 
-git checkout gh-pages
-
-# get name of previously checked out branch
-PREV_BRANCH=$(basename $(git rev-parse --symbolic-full-name @{-1}))
+# get name of current branch before switching
+ORIG_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # get revision of previously checked out branch
 REV=$(git rev-parse @{-1})
 
-MESSAGE="Deploying from ${PREV_BRANCH} rev ${REV}"
+MESSAGE="Deploying from ${ORIG_BRANCH} rev ${REV}"
 
+if [ "${ORIG_BRANCH}" = "gh-pages" ]; then
+    echo "Cannot deploy from gh-pages itself. Change to dev or master."
+    exit 1
+fi
+
+git checkout gh-pages
+
+# Unpack output/
 cp -r output/* ./
-git commit --all --message "${MESSAGE}"
-echo git push origin gh-pages
-git checkout ${PREV_BRANCH}
+
+# http://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommited-changes
+git update-index -q --ignore-submodules --refresh
+
+# check if any files changed and check them in if so
+if ! $(git diff-files --quiet --ignore-submodules); then
+    git commit --all --message "${MESSAGE}" 
+    echo "Pushing to origin gh-pages..."
+    git push origin gh-pages
+else
+    echo "Nothing updated."
+fi
+
+# back to our original branch
+git checkout ${ORIG_BRANCH}
